7c67503bd7f391da0da90a1dfc1cc6b6e3aa0796
Alex <luongalex06@gmail.com>
2024-10-09 13:27:24 -0600

    i finished translating python to js

--- generate_password.js
+++ generate_password.js
@@ -1,5 +1,16 @@
 function generatePassword(length = 12) {
-    // complete me
+// Typed out all of the characters needed to make the password
+    const characters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM,./<>?;':\"[]\\{}|`1234567890-=~!@#$%^&*()_+"
+
+    // Made a new empty string
+let password = '';
+
+// Adds twelve characters into password
+for (let i = 0; i < length; i++) {
+    password += characters.charAt(Math.floor(Math.random() * characters.length));
+}
+return password
+
 }
 
 module.exports = generatePassword;
